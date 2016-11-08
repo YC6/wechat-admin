f4d45235fb0217617b30f2b084d1c64bac81329d
sliwey <qlw1009@gmail.com>
2016-11-08 17:13:36 +0800

    add login

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, Link, browserHistory } from "react-router";
 import Login from "./components/login/login";
-import { Row, Col } from "antd";
 
 class App extends React.Component{
     constructor(props) {
@@ -18,7 +17,7 @@ class App extends React.Component{
 
 ReactDOM.render((
     <Router history={browserHistory}>
-        <Route path="/" component={App} />
+        <Route path="/" component={Login} />
         <Route path="/login" component={Login} />
     </Router>
 ), document.getElementById("app"));
